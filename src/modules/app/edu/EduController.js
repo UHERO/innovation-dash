9e70cb52f18b4cb7b37132adf263c79e9698baf4
Jon Tan <joanthan.tan83@gmail.com>
2015-03-16 19:10:14 -1000

    adds html structure to Views Container

--- src/modules/app/edu/EduController.js
+++ src/modules/app/edu/EduController.js
@@ -3,4 +3,10 @@
 module.exports = /*@ngInject*/
   function EduController($scope) {
     $scope.welcome = 'Education?';
+    $scope.currentyear = '2015';
+    $scope.percent = '16.6%';
+    $scope.major = 'Science & Engineering';
+    $scope.startyear = '1968';
+    $scope.endyear = '2014';
+    $scope.sampledata = '0 - 12.6';
   };
