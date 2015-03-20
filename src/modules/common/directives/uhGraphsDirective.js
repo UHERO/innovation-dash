f3ca373c18a16b69da03be959e63d3b62ec9a1e4
jmt75200 <joelle@pcgamerzhawaii.com>
2015-03-19 17:17:50 -1000

    adding map functionality

--- src/modules/common/directives/uhGraphsDirective.js
+++ src/modules/common/directives/uhGraphsDirective.js
@@ -9,17 +9,16 @@ module.exports = /*@ngInject*/
         //get stuff from controller via bi-directional binding
         d3Data: '='
       },
-      template: '<div class="uh-graphs">Data goes Here<div>',
+      template: '<div class="uh-maps">Data goes Here<div>',
       // templateUrl: 'common/directives/templates/uhGraphs.html',
     
       link: function (scope, element, attrs) {
-        var mapSource = '/assets/images/USA.json'
-        var helloD3 = require('../d3/charts');
-        helloD3("/assets/per_capita_personal_income.csv", ".uh-graphs");
+        // var mapSource = '/assets/images/USA.json';
+        var d3Charts = require('../d3/charts');
+        d3Charts('/assets/maps/USA.json',"/assets/csv/per_capita_personal_income.csv", ".uh-maps");
+        console.log(d3Charts);
        // scope.names = ['jackie', 'jon', 'jesse'];
        // console.log('scope.names', scope.names);
       }
     };
   };
-
- 
