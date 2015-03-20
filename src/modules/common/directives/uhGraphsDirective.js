9ad8c13ef9d36a76b8736da1e1926028734a5d9d
jmt75200 <joelle@pcgamerzhawaii.com>
2015-03-19 19:53:52 -1000

    Added directive template, data being served via api, template has been added to the first view of each tab.

--- src/modules/common/directives/uhGraphsDirective.js
+++ src/modules/common/directives/uhGraphsDirective.js
@@ -9,17 +9,16 @@ module.exports = /*@ngInject*/
         //get stuff from controller via bi-directional binding
         d3Data: '='
       },
-      template: '<div class="uh-maps">Data goes Here<div>',
-      // templateUrl: 'common/directives/templates/uhGraphs.html',
+      // template: '<div class="uh-maps">Data goes Here<div>',
+      templateUrl: 'common/directives/templates/uhGraphs.html',
     
       link: function (scope, element, attrs) {
         // var mapSource = '/assets/images/USA.json';
         var d3Charts = require('../d3/charts');
-        d3Charts('/assets/maps/USA.json',"/assets/csv/per_capita_personal_income.csv", ".uh-maps");
         // d3Charts('/assets/maps/USA.json',"/assets/csv/per_capita_personal_income.csv", ".uh-maps");
+        d3Charts('/assets/maps/USA.json',"http://10.0.1.8:4567/economics/eco_per_capita_personal_income", "#uh-map");
         console.log(d3Charts);
-       // scope.names = ['jackie', 'jon', 'jesse'];
-       // console.log('scope.names', scope.names);
+        
       }
     };
   };
