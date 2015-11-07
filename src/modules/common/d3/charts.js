565258e9e749d23997b8ae78ba505ba4bbb36664
Victoria Ward <vcward@hawaii.rr.com>
2015-11-06 19:04:18 -1000

    add function to plot points on line graphs

--- src/modules/common/d3/charts.js
+++ src/modules/common/d3/charts.js
@@ -629,23 +629,37 @@ module.exports = function (scope, mapSource, dataSource,
       })
       .interpolate("linear");
 
+    // Draw plot points
+    function drawPoints(graphSVG, data, color) {
+      graphSVG.selectAll("dot")
+         .data(data.filter(function(d) { return !isNaN(d.value); }))
+         .enter().append("circle")
+         .attr("r", 2.5)
+         .attr("cx", function(d) { return xScale(d.year); })
+         .attr("cy", function(d) { return yScale(d.value); })
+         .style("fill", color);
+     }
+
     // when there is a US Average data object
     if (datasetSummaryRecords.length !== 0) {
       window.usData = dataByState(filteredStates, knownSummaryRecords[0], geoAreaCategory);
       usAvgData = window.usData;
 
       drawLine(vis, usAvgData, graphColors.usColor);
+      drawPoints(vis, usAvgData, graphColors.usColor);
     }
 
     window.hiData = dataByState(filteredStates, geoAreaNames[0], geoAreaCategory);
     hiStateData = window.hiData;
     drawLine(vis, hiStateData, graphColors.hiColor);
+    drawPoints(vis, hiStateData, graphColors.hiColor);
 
     window.selStateData = dataByState(filteredStates, geoAreaNames[1], geoAreaCategory);
     selectedStateData =  window.selStateData;
 
     if (selectedStateData.length !== 0) {
       drawLine(vis, selectedStateData, graphColors.selectedColor);
+      drawPoints(vis, selectedStateData, graphColors.selectedColor);
     }
     /* END OF LINE DRAWINGS */
 
@@ -1042,6 +1056,7 @@ module.exports = function (scope, mapSource, dataSource,
        .attr("stroke", color)
        .attr("stroke-width", 3)
        .attr("fill", "none");
+
   } //end drawLine
 
   function findGeoValueAtYear (lineData, year) {
